e429793ca37aaedcf2b19d97b7d33665e831c242
Anuoluwapo <jaguarjum@gmail.com>
2023-03-12 18:36:51 +0100

    Scrollbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 
 import './App.css';
 import Navbar from "./components/Navbar"
-
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      {/* <Navbar/> */}
+      <Sidebar/>
     </div>
   );
 }
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,19 +1,43 @@
-import React from 'react';
+import React from "react";
 import CloseIcon from "@mui/icons-material/Close";
 
 const Sidebar = () => {
   return (
     <div className="Sidebar_container">
       <div className="Sidebar_wrapper">
-        <span className='Sidebar-title'>
-         
+        <span className="Sidebar_title">
           <h3>MENU</h3>
 
-          <i> <CloseIcon/> </i>
+          <i>
+            <CloseIcon />
+          </i>
         </span>
+
+        <div className="Sidebar_menu">
+          <h3>Shop By Department</h3>
+          <p>Smart Phones</p>
+          <p>Televisions</p>
+          <p>Laptops & Computers</p>
+          <p>Speakers</p>
+          <p>Video Games </p>
+
+          <h3>Sections</h3>
+          <p>Shop</p>
+          <p>Products</p>
+          <p>About Us</p>
+          <p>Contact Us</p>
+
+          <h3>Support</h3>
+          <p>Visit Our Support</p>
+          <p>online Support</p>
+
+          <span className="Logout">
+            <p>LogOut</p>
+          </span>
+        </div>
       </div>
     </div>
   );
-}
+};
 
-export default Sidebar
+export default Sidebar;
